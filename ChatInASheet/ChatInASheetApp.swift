89ed07b12879f8f7c5721326b502e18c3d13e5d8
Michael Freiwald <michael@freiwald.dev>
2023-09-22 10:39:50 +0200

    Initial Commit

---
+++ ChatInASheet/ChatInASheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatInASheetApp.swift
+//  ChatInASheet
+//
+//  Created by Michael on 22.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ChatInASheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
